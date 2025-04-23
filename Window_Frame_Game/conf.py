67750f14a3f1d5b04d4ad2e0f5a12f77c1b4e8b5
halilibrahimyesirci <halilibrahimyesirci@gmail.com>
2025-04-23 20:13:41 +0300

    d
    d

--- Window_Frame_Game/conf.py
+++ Window_Frame_Game/conf.py
@@ -1,25 +1,25 @@
-# Window configurations
+
 PLAYER_WINDOW_TITLE = "Hunter"
 PLAYER_WINDOW_SIZE = (100, 100)
 PLAYER_WINDOW_COLOR = "black"
 PLAYER_WINDOW_SPEED = 5
 
-# Target window configurations
+
 TARGET_WINDOW_COUNT = 5
 TARGET_WINDOW_TITLE = "Target"
 TARGET_WINDOW_SIZE = (200, 150)
 TARGET_WINDOW_COLORS = ["red", "blue", "green", "yellow", "purple"]
 
-# Game settings
+
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 GAME_TITLE = "Window Hunter"
 SCORE_INCREMENT = 10
 
-# FPS settings
-TARGET_FPS = 60
-UPDATE_INTERVAL = 1000 // TARGET_FPS  # in milliseconds
 
-# Shape configurations
-SHAPE_TYPES = ["rectangle", "triangle"]  # Available shape types
-WINDOW_ALPHA = 0.8  # Window transparency (0.0 to 1.0)
+TARGET_FPS = 60
+UPDATE_INTERVAL = 1000 // TARGET_FPS  
+
+
+SHAPE_TYPES = ["rectangle", "triangle"] 
+WINDOW_ALPHA = 0.8  
